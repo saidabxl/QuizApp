e0d342554f8c7dd7c9b4d740e39379f4b621f728
saidabxl <qunsulka@gmail.com>
2018-01-13 15:53:24 +0100

    Initial commit

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -16,30 +16,51 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //general variables that store information
+    RadioGroup radioGroup1;
+    RadioGroup radioGroup2;
+    RadioGroup radioGroup3;
+    RadioButton question1answer1_RadioButton;
+    RadioButton question1answer2_RadioButton;
+    RadioButton question1answer3_RadioButton;
+    RadioButton question2answer1_RadioButton;
+    RadioButton question2answer2_RadioButton;
+    RadioButton question2answer3_RadioButton;
+    RadioButton question3answer1_RadioButton;
+    RadioButton question3answer2_RadioButton;
+    RadioButton question3answer3_RadioButton;
+    CheckBox question4answer1_CheckBox;
+    CheckBox question4answer2_CheckBox;
+    CheckBox question4answer3_CheckBox;
+    CheckBox question5answer1_CheckBox;
+    CheckBox question5answer2_CheckBox;
+    CheckBox question5answer3_CheckBox;
+    EditText nameInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RadioGroup radioGroup1 = ( RadioGroup ) findViewById(R.id.question1radioGroup);
-        RadioGroup radioGroup2 = ( RadioGroup ) findViewById(R.id.question2radioGroup);
-        RadioGroup radioGroup3 = ( RadioGroup ) findViewById(R.id.question3radioGroup);
-        RadioButton question1answer1_RadioButton = findViewById(R.id.question1answer1);
-        RadioButton question1answer2_RadioButton = findViewById(R.id.question1answer2);
-        RadioButton question1answer3_RadioButton = findViewById(R.id.question1answer3);
-        RadioButton question2answer1_RadioButton = findViewById(R.id.question2answer1);
-        RadioButton question2answer2_RadioButton = findViewById(R.id.question2answer2);
-        RadioButton question2answer3_RadioButton = findViewById(R.id.question2answer3);
-        RadioButton question3answer1_RadioButton = findViewById(R.id.question3answer1);
-        RadioButton question3answer2_RadioButton = findViewById(R.id.question3answer2);
-        RadioButton question3answer3_RadioButton = findViewById(R.id.question3answer3);
-        CheckBox question4answer1_CheckBox = findViewById(R.id.question4answer1);
-        CheckBox question4answer2_CheckBox = findViewById(R.id.question4answer2);
-        CheckBox question4answer3_CheckBox = findViewById(R.id.question4answer3);
-        CheckBox question5answer1_CheckBox = findViewById(R.id.question5answer1);
-        CheckBox question5answer2_CheckBox = findViewById(R.id.question5answer2);
-        CheckBox question5answer3_CheckBox = findViewById(R.id.question5answer3);
-        EditText nameInput = ( EditText ) findViewById(R.id.firstAnswer);
+        radioGroup1 = ( RadioGroup ) findViewById(R.id.question1radioGroup);
+        radioGroup2 = ( RadioGroup ) findViewById(R.id.question2radioGroup);
+        radioGroup3 = ( RadioGroup ) findViewById(R.id.question3radioGroup);
+        question1answer1_RadioButton = findViewById(R.id.question1answer1);
+        question1answer2_RadioButton = findViewById(R.id.question1answer2);
+        question1answer3_RadioButton = findViewById(R.id.question1answer3);
+        question2answer1_RadioButton = findViewById(R.id.question2answer1);
+        question2answer2_RadioButton = findViewById(R.id.question2answer2);
+        question2answer3_RadioButton = findViewById(R.id.question2answer3);
+        question3answer1_RadioButton = findViewById(R.id.question3answer1);
+        question3answer2_RadioButton = findViewById(R.id.question3answer2);
+        question3answer3_RadioButton = findViewById(R.id.question3answer3);
+        question4answer1_CheckBox = findViewById(R.id.question4answer1);
+        question4answer2_CheckBox = findViewById(R.id.question4answer2);
+        question4answer3_CheckBox = findViewById(R.id.question4answer3);
+        question5answer1_CheckBox = findViewById(R.id.question5answer1);
+        question5answer2_CheckBox = findViewById(R.id.question5answer2);
+        question5answer3_CheckBox = findViewById(R.id.question5answer3);
+        nameInput = ( EditText ) findViewById(R.id.firstAnswer);
 
     }
 
@@ -65,47 +86,31 @@ public class MainActivity extends AppCompatActivity {
         };
 
         // Get the name of user
-        EditText nameInput = ( EditText ) findViewById(R.id.firstAnswer);
         String userName = nameInput.getText().toString();
 
         //1 question - take answers to array
-        RadioButton question1answer1_RadioButton = findViewById(R.id.question1answer1);
         userCheckAnswers[0][0] = question1answer1_RadioButton.isChecked();
-        RadioButton question1answer2_RadioButton = findViewById(R.id.question1answer2);
         userCheckAnswers[0][1] = question1answer2_RadioButton.isChecked();
-        RadioButton question1answer3_RadioButton = findViewById(R.id.question1answer3);
         userCheckAnswers[0][2] = question1answer3_RadioButton.isChecked();
 
         //2 question - take answers to array
-        RadioButton question2answer1_RadioButton = findViewById(R.id.question2answer1);
         userCheckAnswers[1][0] = question2answer1_RadioButton.isChecked();
-        RadioButton question2answer2_RadioButton = findViewById(R.id.question2answer2);
         userCheckAnswers[1][1] = question2answer2_RadioButton.isChecked();
-        RadioButton question2answer3_RadioButton = findViewById(R.id.question2answer3);
         userCheckAnswers[1][2] = question2answer3_RadioButton.isChecked();
 
         //3 question - take answers to array
-        RadioButton question3answer1_RadioButton = findViewById(R.id.question3answer1);
         userCheckAnswers[2][0] = question3answer1_RadioButton.isChecked();
-        RadioButton question3answer2_RadioButton = findViewById(R.id.question3answer2);
         userCheckAnswers[2][1] = question3answer2_RadioButton.isChecked();
-        RadioButton question3answer3_RadioButton = findViewById(R.id.question3answer3);
         userCheckAnswers[2][2] = question3answer3_RadioButton.isChecked();
 
         //4 question - take answers to array
-        CheckBox question4answer1_CheckBox = findViewById(R.id.question4answer1);
         userCheckAnswers[3][0] = question4answer1_CheckBox.isChecked();
-        CheckBox question4answer2_CheckBox = findViewById(R.id.question4answer2);
         userCheckAnswers[3][1] = question4answer2_CheckBox.isChecked();
-        CheckBox question4answer3_CheckBox = findViewById(R.id.question4answer3);
         userCheckAnswers[3][2] = question4answer3_CheckBox.isChecked();
 
         //5 question - take answers to array
-        CheckBox question5answer1_CheckBox = findViewById(R.id.question5answer1);
         userCheckAnswers[4][0] = question5answer1_CheckBox.isChecked();
-        CheckBox question5answer2_CheckBox = findViewById(R.id.question5answer2);
         userCheckAnswers[4][1] = question5answer2_CheckBox.isChecked();
-        CheckBox question5answer3_CheckBox = findViewById(R.id.question5answer3);
         userCheckAnswers[4][2] = question5answer3_CheckBox.isChecked();
 
         //Checking arrays and making toast
